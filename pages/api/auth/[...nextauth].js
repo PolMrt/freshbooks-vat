496f78817224f23e67dc2ee19b86e5b315b5396c
Pol Marnette <pol.marnette@gmail.com>
2021-05-28 21:41:29 +0200

    [+] init

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,18 @@
+import NextAuth from 'next-auth'
+import FreshbooksProvider from '../../../lib/freshbooksProvider'
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    FreshbooksProvider({
+      clientId: process.env.FRESHBOOKS_CLIENT_ID,
+      clientSecret: process.env.FRESHBOOKS_CLIENT_SECRET,
+      redirectUri: 'https://localhost:3000/api/auth/callback/freshbooks'
+    })
+    // ...add more providers here
+  ],
+  debug: true
+
+  // A database is optional, but required to persist accounts in a database
+  // database: process.env.DATABASE_URL,
+})
