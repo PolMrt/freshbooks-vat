0c188413ea3f8ccd9dfb0807556c3c20d166c534
Pol Marnette <pol.marnette@gmail.com>
2021-05-31 19:08:14 +0200

    [~] Calculate vat on income

--- pages/api/req.js
+++ pages/api/req.js
@@ -4,12 +4,14 @@ export default async function helloAPI(req, res) {
   try {
     const session = await getSession({ req }) //r7owdo
     if (session) {
+      const belgiumVatName = 'TVA BE'
+      const intracomVatName = 'TVA Intracom'
+
       const dataRaw = await fetch(
-        /* 'https://api.freshbooks.com/accounting/account/r7owdo/reports/accounting/invoice_details?start_date=2021-01-01&end_date=2021-03-31' */
-        'https://api.freshbooks.com/accounting/account/r7owdo/expenses/expenses',
+        'https://api.freshbooks.com/accounting/account/r7owdo/reports/accounting/invoice_details?start_date=2021-01-01&end_date=2021-03-31',
         {
           headers: {
-            Authorization: `Bearer ...`,
+            Authorization: `Bearer ${process.env.Bearer}`,
             'Api-Version': `alpha`,
             'Content-Type': `application/json`
           }
@@ -18,7 +20,67 @@ export default async function helloAPI(req, res) {
 
       const data = await dataRaw.json()
 
-      res.status(200).json({ a: data, s: session })
+      const totaleTVAOnSales = data.response.result.invoice_details.clients.reduce((prev, current) => {
+        return (
+          prev +
+          current.invoices.reduce((previousInvoice, currentInvoice) => {
+            return (
+              previousInvoice +
+              currentInvoice.tax_summaries
+                .filter((thisTax) => thisTax.tax_name === belgiumVatName)
+                .reduce((previousTax, currentTax) => previousTax + parseFloat(currentTax.tax_total.amount), 0)
+            )
+          }, 0)
+        )
+      }, 0)
+
+      const totaleSalesIntracom = data.response.result.invoice_details.clients.reduce((prev, current) => {
+        return (
+          prev +
+          current.invoices.reduce((previousInvoice, currentInvoice) => {
+            return (
+              previousInvoice +
+              currentInvoice.lines
+                .filter((line) => line.tax_name1 === intracomVatName || line.tax_name2 === intracomVatName)
+                .reduce((previousLine, currentLine) => {
+                  return previousLine + parseFloat(currentLine.total.amount)
+                }, 0)
+            )
+          }, 0)
+        )
+      }, 0)
+
+      const [totaleSalesWithoutTVA6, totaleSalesWithoutTVA12, totaleSalesWithoutTVA21] = ['6', '12', '21'].map(
+        (TVARate) =>
+          data.response.result.invoice_details.clients.reduce((prev, current) => {
+            return (
+              prev +
+              current.invoices.reduce((previousInvoice, currentInvoice) => {
+                return (
+                  previousInvoice +
+                  currentInvoice.tax_summaries
+                    // .filter((thisTax) => thisTax.tax_name === belgiumVatName)
+                    .filter((thisTax) => thisTax.tax_rate === TVARate)
+                    .reduce(
+                      (previousTax, currentTax) =>
+                        previousTax + parseFloat(currentTax.tax_total.amount) * (1 / (parseFloat(TVARate) / 100)),
+                      0
+                    )
+                )
+              }, 0)
+            )
+          }, 0)
+      )
+
+      res.status(200).json({
+        54: totaleTVAOnSales,
+        '01': totaleSalesWithoutTVA6,
+        '02': totaleSalesWithoutTVA12,
+        '03': totaleSalesWithoutTVA21,
+        44: totaleSalesIntracom,
+        XX: totaleTVAOnSales,
+        d: data
+      })
     } else {
       res.status(401).json({ error: 'not authorized' })
     }
